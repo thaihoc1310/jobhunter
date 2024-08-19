4e460a241ddf256967a88c8505fdc8d48d5b6921
thaihoc <thaihoc131005@gmail.com>
2024-08-19 11:16:09 +0700

    create base project

---
+++ src/main/java/vn/thaihoc/jobhunter/controller/HelloController.java
@@ -0,0 +1,13 @@
+package vn.thaihoc.jobhunter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String getHelloWorld() {
+        return "Hello World , this is job-hunter project";
+    }
+}
