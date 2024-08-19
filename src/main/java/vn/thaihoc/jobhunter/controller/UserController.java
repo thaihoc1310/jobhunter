2b5c2625fc231ccc98361231d869997000408701
thaihoc <thaihoc131005@gmail.com>
2024-08-19 21:17:48 +0700

    use pathvariable to code function deleteUser

--- src/main/java/vn/thaihoc/jobhunter/controller/UserController.java
+++ src/main/java/vn/thaihoc/jobhunter/controller/UserController.java
@@ -5,9 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 import vn.thaihoc.jobhunter.domain.User;
 import vn.thaihoc.jobhunter.service.UserService;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class UserController {
@@ -18,8 +21,14 @@ public class UserController {
     }
 
     // @GetMapping("/user/create")
-    @PostMapping("/user/create")
+    @PostMapping("/user")
     public User createNewUser(@RequestBody User user) {
         return this.userService.handleCreateUser(user);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        this.userService.handleDeleteUserById(id);
+        return "Delete user";
     }
 }
