6ba3bdf48680672bdb5573a5efe4b614d4bfcb0a
thaihoc <thaihoc131005@gmail.com>
2024-08-21 17:30:31 +0700

    create RestResponse Object and adjust it by GlobalException to Format Response

--- src/main/java/vn/thaihoc/jobhunter/service/error/GlobalExeption.java
+++ src/main/java/vn/thaihoc/jobhunter/service/error/GlobalExeption.java
@@ -1,14 +1,21 @@
 package vn.thaihoc.jobhunter.service.error;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import vn.thaihoc.jobhunter.domain.RestResponse;
+
 @RestControllerAdvice
 public class GlobalExeption {
     @ExceptionHandler(value = IdInvalidException.class)
-    public ResponseEntity<String> handleIdInvalidException(IdInvalidException idException) {
-        return ResponseEntity.badRequest().body(idException.getMessage());
+    public ResponseEntity<RestResponse<Object>> handleIdInvalidException(IdInvalidException idException) {
+        RestResponse<Object> res = new RestResponse<>();
+        res.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        res.setError(idException.getMessage());
+        res.setMessage("IdInvalidException");
+        return ResponseEntity.badRequest().body(res);
     }
 
 }
