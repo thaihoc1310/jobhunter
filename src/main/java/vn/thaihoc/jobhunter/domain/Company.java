e6790c905810caf9776b9d3b3dfa5aa70a857235
thaihoc <thaihoc131005@gmail.com>
2024-09-23 08:39:33 +0700

    add relationship between User and Company

--- src/main/java/vn/thaihoc/jobhunter/domain/Company.java
+++ src/main/java/vn/thaihoc/jobhunter/domain/Company.java
@@ -2,11 +2,15 @@ package vn.thaihoc.jobhunter.domain;
 
 import java.time.Instant;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -43,6 +47,9 @@ public class Company {
     private String createdBy;
 
     private String updatedBy;
+
+    @OneToMany(mappedBy = "company", fetch = FetchType.LAZY)
+    List<User> users;
 
     @PrePersist
     public void handleBeforeCreate() {
